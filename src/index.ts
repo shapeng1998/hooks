61b9295f7284274178c3f7ab05e3e1311651a69a
Peng Sha <me@pengsha.dev>
2024-11-26 14:34:55 +0800

    feat: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { useDebouncedValue } from './use-debounced-value/use-debounced-value';
 export { useAbortableEffect } from './use-abortable-effect/use-abortable-effect';
+export { useEvent } from './use-event/use-event.js';
